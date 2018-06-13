700aa598c26308e0a7add6869505c271a3554392
Saikat <saikats_5@yahoo.co.in>
2018-06-13 08:28:06 +0530

    palindrome solution 2 commit

--- palindrome/index.js
+++ palindrome/index.js
@@ -5,3 +5,9 @@ function palindrome(str){
     const reversed = str.split('').reverse().join('');
     return str === reversed;
 }
+
+function palindrome(str){
+    return str.split('').every((char, i)=>{
+        return char === str[str.length - i - 1];
+    });
+}
