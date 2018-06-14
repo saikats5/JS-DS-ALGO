deefa3df642d21ae383f50024c9cf5e22baada6e
Saikat <saikats_5@yahoo.co.in>
2018-06-14 08:47:47 +0530

    Solution 2 to find max char

--- maxchar/index.js
+++ maxchar/index.js
@@ -1,12 +1,24 @@
 const string = "Hello World";
 const chars = {};
 
+let max = 0;
+let maxChar = '';
+
 for(let char of string){
     if(!chars[char]){
         chars[char] = 1;
     }else{
         chars[char]++;
     }
+
+    //Max Char Iteration
+    for(let char in chars){
+        if(chars[char] > max){
+            max = chars[char];
+            maxChar = char;
+        }
+    }
+
 }
 
 for(let char of string){
